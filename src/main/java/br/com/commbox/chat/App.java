456b4b7ce6c6dfb7e956d10a9a5929a26e4a4fc2
Ricardo Sander Lopes <ricardo.sander.lopes@gmail.com>
2016-12-13 15:04:19 -0200

    Iniciando projeto.

---
+++ src/main/java/br/com/commbox/chat/App.java
@@ -0,0 +1,13 @@
+package br.com.commbox.chat;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
