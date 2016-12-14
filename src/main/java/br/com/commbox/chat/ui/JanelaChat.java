fd22b497f7352b899626e999e4cc2f9f5fd44cc0
Ricardo Sander Lopes <ricardo.sander.lopes@gmail.com>
2016-12-14 14:46:59 -0200

    Implementando novos comportamentos da JanelaChat conforme interface.

--- src/main/java/br/com/commbox/chat/ui/JanelaChat.java
+++ src/main/java/br/com/commbox/chat/ui/JanelaChat.java
@@ -15,29 +15,29 @@ public class JanelaChat implements Janela {
 
 	private static final String QUEBRA_DE_LINHA = "\n";
 	private static final String TITULO_JANELA = "Chat Commbox";
-	
+
 	private JFrame janela;
-	
+
 	private JPanel container;
 	private JPanel painelBotoes;
 	private JPanel painelMensagem;
 	private JPanel painelChat;
-	
+
 	private JButton botaoEnviar;
 	private JButton botaoLimpar;
 	private JButton botaoSair;
-	
+
 	private JTextArea textoChat;
 	private JTextArea textoMensagem;
 
 	public JanelaChat() {
-		
+
 		this.prepararJanela();
 		this.prepararPaineis();
 		this.prepararCaixasTexto();
 		this.prepararBotoes();
 	}
-	
+
 	public void abrir() {
 
 		this.janela.pack();
@@ -46,56 +46,59 @@ public class JanelaChat implements Janela {
 	}
 
 	private void prepararCaixasTexto() {
-		
+
 		textoChat = new JTextArea(10, 10);
 		textoMensagem = new JTextArea(2, 10);
-		
+
 		textoChat.setEditable(false);
-		
+
 		textoMensagem.setLineWrap(true);
-		
+
 		JScrollPane scrollPaneChat = new JScrollPane(this.textoChat);
 		scrollPaneChat.setPreferredSize(new Dimension(500, 200));
-		
+
 		JScrollPane scrollPaneMessage = new JScrollPane(this.textoMensagem);
 		scrollPaneMessage.setPreferredSize(new Dimension(500, 50));
-		
+
 		this.painelChat.add(scrollPaneChat);
 		this.painelMensagem.add(scrollPaneMessage);
 	}
 
+	private synchronized void enviaMensagem() {
+
+		String conteudo = textoMensagem.getText();
+		if (conteudo == null || conteudo.trim().isEmpty()) {
+			return;
+		}
+		this.notify();
+	}
+
 	private void prepararBotoes() {
-		
+
 		botaoEnviar = new JButton("Enviar");
 		botaoEnviar.addActionListener(new ActionListener() {
-			
+
 			public void actionPerformed(ActionEvent e) {
-				
-				String conteudo = textoMensagem.getText();
-				if (conteudo == null || conteudo.trim().isEmpty()) {
-					return;
-				}
-				textoChat.append("Você: " + conteudo.trim() + JanelaChat.QUEBRA_DE_LINHA);
-				textoMensagem.setText(null);
+				enviaMensagem();
 			}
 		});
-		
+
 		botaoLimpar = new JButton("Limpar");
 		botaoLimpar.addActionListener(new ActionListener() {
-			
+
 			public void actionPerformed(ActionEvent e) {
 				textoMensagem.setText(null);
 			}
 		});
-		
+
 		botaoSair = new JButton("Sair");
 		botaoSair.addActionListener(new ActionListener() {
-			
+
 			public void actionPerformed(ActionEvent e) {
 				System.exit(0);
 			}
 		});
-		
+
 		this.painelBotoes.add(this.botaoSair);
 		this.painelBotoes.add(this.botaoLimpar);
 		this.painelBotoes.add(this.botaoEnviar);
@@ -109,23 +112,53 @@ public class JanelaChat implements Janela {
 	}
 
 	private void prepararPaineis() {
-		
+
 		this.container = new JPanel();
 		this.container.setLayout(new BoxLayout(container, BoxLayout.Y_AXIS));
 		this.janela.add(container);
-		
+
 		this.painelChat = new JPanel();
 		this.painelMensagem = new JPanel();
 		this.painelBotoes = new JPanel();
-		
+
 		this.container.add(this.painelChat);
 		this.container.add(this.painelMensagem);
 		this.container.add(this.painelBotoes);
 	}
-	
+
 	public static void main(String[] args) {
-		
+
 		JanelaChat janela = new JanelaChat();
 		janela.abrir();
+	}
+
+	@Override
+	public void fechar() {
+		this.janela.setVisible(false);
+	}
+
+	@Override
+	public String getMensagem() {
+
+		synchronized (this) {
+			try {
+				this.wait();
+
+				String mensagem = this.textoMensagem.getText();
+
+				textoMensagem.setText(null);
+
+				return mensagem;
+
+			} catch (InterruptedException e) {
+				System.out.println("Houve um erro no envio de mensagem: " + e.getMessage());
+			}
+		}
+		throw new RuntimeException("Houve um erro no chat.");
+	}
+
+	@Override
+	public void escreve(String mensagem) {
+		this.textoChat.append(QUEBRA_DE_LINHA + mensagem);
 	}
 }
